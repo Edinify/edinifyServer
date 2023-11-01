04098431a4b1a7a929e318947b2b2f78236eee09
SamedovResul <samedovrasul7@gmail.com>
2023-11-01 11:27:07 +0400

    logs

--- config/logger.js
+++ config/logger.js
@@ -2,7 +2,7 @@ import winston, { format } from "winston";
 
 const logger = winston.createLogger({
   level: "error",
-  transports: [new winston.transports.File({ filename: "logs/error.log" })],
+  transports: [new winston.transports.File({ filename: "error.log" })],
   format: format.combine(format.timestamp(), format.json()),
 });
 
