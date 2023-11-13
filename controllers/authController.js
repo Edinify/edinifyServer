44b92c1a4e152808248a6b0708f1647f926f3c94
SamedovResul <samedovrasul7@gmail.com>
2023-11-13 11:00:16 +0400

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -254,7 +254,8 @@ export const login = async (req, res) => {
     res.cookie("refreshtoken", RefreshToken, {
       httpOnly: true,
       path: "/api/user/auth/refresh_token",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
+      sameSite: "None",
+      secure: true,
     });
 
     res.status(200).json({
