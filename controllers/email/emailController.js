115145143ecbcd7ca0c231265cb9ddc133792a5f
SamedovResul <samedovrasul7@gmail.com>
2023-12-28 11:42:57 +0400

    .

--- controllers/email/emailController.js
+++ controllers/email/emailController.js
@@ -4,13 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const emailSender = async (req,res) =>{
-  const {companyName,
-  studentCount,
-  name,
-  surName,
-  email,
-  phone,
-  contactTime,} = req.body;
+  const {
+    features,
+    companyName,
+    studentCount,
+    name,
+    surName,
+    email,
+    phone,
+    contactTime,
+  } = req.body;
   try {
     console.log(req.body)
     const mainEmail = process.env.EMAIL;
@@ -31,14 +34,17 @@ export const emailSender = async (req,res) =>{
       to: "samedovrasul7@gmail.com",
       subject: " from Edinify ",
       html:  `<ul>
+          <p> İstənilən xüsusiyyətlər, </p>
+          <li>   ${ features.map((data) =>  `${data} /`) } </li>
           <p> Şirkət haqqında </p>
-          <li> <b> Şirkət adı </b>  ${companyName} </li>
-          <li> <b> Tələbə sayı </b>  ${studentCount} </li>
+          <li> <b> Şirkət adı: </b>  ${companyName} </li>
+          <li> <b> Tələbə sayı: </b>  ${studentCount} </li>
           <p> Əlaqə məlumatı </p>
-          <li> <b> Ad </b>  ${name} </li>
-          <li> <b> Soyad </b>  ${surName} </li>
-          <li> <b> Email </b>  ${email} </li>
-          <li> <b> Telefon </b>  ${phone} </li>
+          <li> <b> Ad: </b>  ${name} </li>
+          <li> <b> Soyad: </b>  ${surName} </li>
+          <li> <b> Email: </b>  ${email} </li>
+          <li> <b> Telefon: </b>  ${phone} </li>
+          <li> <b> Əlaqə zamanları: </b>  ${ contactTime.map((data) =>  `${data} /`) } </li>
         </ul>`
       ,
     };
