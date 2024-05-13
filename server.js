6a8bc6eef514385d4891b34e4ff1835066542835
SamedovResul <samedovrasul7@gmail.com>
2024-05-13 12:21:30 +0400

    change origin

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ const uri = process.env.DB_URI;
 
 app.use(
   cors({
-    origin: process.env.URL_PORT,
-    credentials: true,
+    origin: "*",
+    // credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
     exposedHeaders: ["Content-Type"],
