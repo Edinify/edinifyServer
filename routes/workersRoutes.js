b6da62ab5cfe59a74d81e8ed621a398b4a041936
xeyyamomarov <xeyyamomarov2014@gmail.com>
2024-07-25 12:40:42 +0400

    worker pagination was added

--- routes/workersRoutes.js
+++ routes/workersRoutes.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
   getWorkers,
   updateWorker,
-  deleteWorker
+  deleteWorker,
+  getWorkersForPagination
 } from "../controllers/workerController.js";
 import {
   authMiddleware,
@@ -13,6 +14,7 @@ import {
 const router = express.Router();
 
 router.get("/", authMiddleware, checkSuperAdmin, getWorkers);
+router.get("/pagination",authMiddleware,getWorkersForPagination,checkSuperAdmin)
 router.patch("/:id", authMiddleware, checkSuperAdmin, updateWorker);
 // router.patch(
 //   "/me/password",
